e23e6ce7da4f91408e8f9d8014e96b4d7722d1ec
Karl Lowe <247839965+karllowe@users.noreply.github.com>
2026-02-18 10:25:27 +0000

    basic file structure added

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const app = express();
+const path = require("node:path");
+const assetsPath = path.join(__dirname, "public");
+const indexRouter = require("./routes/indexRouter");
+const PORT = 3000;
+
+app.use(express.static(assetsPath));
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.use("/", indexRouter);
+
+app.use((err, req, res, next) => {
+  console.error(err);
+  res.status(err.statusCode || 500).send(err.message);
+});
+app.listen(PORT, (error) => {
+  if (error) {
+    throw error;
+  }
+  console.log(`Listening on Port: ${PORT}`);
+});
