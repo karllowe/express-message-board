55463fc7be20376706638db14b387744df925a8e
Karl Lowe <247839965+karllowe@users.noreply.github.com>
2026-02-18 11:14:17 +0000

    added basic functionality
    messages can be created and viewed

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const PORT = 3000;
 app.use(express.static(assetsPath));
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
+app.use(express.urlencoded({extended: true}));
 
 app.use("/", indexRouter);
 
--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -1,6 +1,31 @@
 const {Router} = require("express");
 const indexRouter = Router();
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date(),
+    id: crypto.randomUUID()
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date(),
+    id: crypto.randomUUID()
+  }
+];
 
-indexRouter.get("/", (req, res) => {res.render("index")});
+indexRouter.get("/", (req, res) => {res.render("index", {messages: messages})});
+indexRouter.get("/new", (req,res) => {res.render("form")});
+indexRouter.post("/new", (req,res) => {
+    messages.push({text: req.body.message_text, user: req.body.author_name, added: new Date(), id: crypto.randomUUID()});
+    res.redirect("/")
+});
+indexRouter.get("/messageDetails/:id", (req,res) => {
+    const id = req.params.id;
+    const message = messages.find((m) => m.id === id);
+
+    res.render("messageDetails", {message, id})
+});
 
 module.exports = indexRouter;
