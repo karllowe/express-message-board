f64a2fc01c86a5b1dd5d1b8191df93b18e98bbf0
Karl Lowe <247839965+karllowe@users.noreply.github.com>
2026-02-23 12:56:54 +0000

    changed db references to message_board

--- db/queries.js
+++ db/queries.js
@@ -1,12 +1,12 @@
 const pool = require("./pool");
 
 async function getMessages() {
-    const {rows} = await pool.query("SELECT * FROM messages INNER JOIN users ON messages.user_id = users.id");
+    const {rows} = await pool.query("SELECT * FROM message_board INNER JOIN users ON message_board.user_id = users.id");
     return rows;
 }
 
 async function getMessageDetails(id) {
-    const { rows } = await pool.query("SELECT * FROM messages INNER JOIN users ON messages.user_id = users.id WHERE messages.id = $1", [id]);
+    const { rows } = await pool.query("SELECT * FROM message_board INNER JOIN users ON message_board.user_id = users.id WHERE message_board.id = $1", [id]);
     return rows[0];
 }
 
@@ -16,7 +16,7 @@ async function getUsers() {
 }
 
 async function postMessage(message, userID, date) {
-    await pool.query("INSERT INTO messages (message, date_added, user_id) values ($1, $2, $3)", [message, date, userID])
+    await pool.query("INSERT INTO message_board (message, date_added, user_id) values ($1, $2, $3)", [message, date, userID])
 }
 
 module.exports = {
